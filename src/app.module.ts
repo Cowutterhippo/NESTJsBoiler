596f943ff4190d90705ddbf5347580d781ff7360
Andrew Kolansky <Andrew.Kolansky@gmail.com>
2022-12-20 18:24:19 -0700

    add modules to App.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,16 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PhotosModule } from './photos/photos.module';
 @Module({
   imports: [
-    UserModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       debug: true,
       playground: true,
     }),
+    PhotosModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
